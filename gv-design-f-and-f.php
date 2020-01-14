ec8c234267a1f5cc2291ae76f0275f67b155007f
Hannah Reilly <hannahreillyca@gmail.com>
2020-01-13 21:44:22 -0800

    Update gv-design-f-and-f.php

--- gv-design-f-and-f.php
+++ gv-design-f-and-f.php
@@ -12,27 +12,25 @@ Author URI: https://github.com/hreilly/
 
 // define AJAX hooks for action
 
-// add_action( 'wp_ajax_gvff_plan_init', 'gvff_plan_init' );
-// add_action( 'wp_ajax_nopriv_gvff_plan_init', 'gvff_plan_init' );
+add_action( 'wp_ajax_gvff_plan_init', 'gvff_plan_init' );
+add_action( 'wp_ajax_nopriv_gvff_plan_init', 'gvff_plan_init' );
 
-// function gvff_plan_init(){
-  
-//   global $wp_query;
-//   wp_enqueue_script( 'gvff_plan_script', plugin_dir_url( __FILE__ ) . '/js/script.js', array(), '1.0', true );
+function gvff_plan_init(){
+   global $wp_query;
+   wp_enqueue_script( 'gvff_plan_script', plugin_dir_url( __FILE__ ) . '/js/script.js', array(), '1.0', true );
 
-//   $max = $wp_query->max_num_pages;
-//   $paged = ( get_query_var('paged') > 1 ) ? get_query_var('paged') : 1;
+   $max = $wp_query->max_num_pages;
+   $paged = ( get_query_var('paged') > 1 ) ? get_query_var('paged') : 1;
 
-//   Pass PHP variables and functions to JS
-//   wp_localize_script( 'gvff_plan_script', 'gvff_plan_vars', array(
-//		 'startPage'  => $paged,
-//		 'maxPages'   => $max,
-//		 'pagination' => gv_pagination(),
-//     'ajax_url'   => admin_url('admin-ajax.php'),
-//	    )
-//   );
-
-// }
-// add_action('template_redirect', 'gvff_plan_init');
+// Pass PHP variables and functions to JS
+   wp_localize_script( 'gvff_plan_script', 'gvff_plan_vars', array(
+		 'startPage'  => $paged,
+		 'maxPages'   => $max,
+  	 'pagination' => gv_pagination(),
+     'ajax_url'   => admin_url('admin-ajax.php'),
+	    )
+   );
+}
+add_action('template_redirect', 'gvff_plan_init');
 
 ?>
