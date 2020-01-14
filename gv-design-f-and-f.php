d0d0bc643d1b1bdd204ec80cf7a08ae1dae9881e
Hannah Reilly <hannahreillyca@gmail.com>
2020-01-13 19:49:02 -0800

    Create gv-design-f-and-f.php

---
+++ gv-design-f-and-f.php
@@ -0,0 +1,10 @@
+<?php
+   /*
+   Plugin Name: GV Design Filters & Functions
+   Plugin URI: https://github.com/hreilly/gv-design-f-and-f/
+   description: Filters and functions for the GV Design website, including AJAX query modifications and pagination.
+   Version: 1.0
+   Author: Hannah Reilly
+   Author URI: https://github.com/hreilly/
+   */
+?>
